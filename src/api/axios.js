3b789ab0dc456028f1fd5c96f70190a4457947ec
nkhatri558 <nkhatri558@gmail.com>
2024-08-07 22:40:30 +0200

    Loadbalcner API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/api'
-    //baseURL: 'http://centralloadbalancer-1492291481.eu-central-1.elb.amazonaws.com/api',
+    baseURL: 'http://centralloadbalancer-1924056286.eu-central-1.elb.amazonaws.com:8080/api',
 });
