e9916365aeeaa21210675f83b108cd778e3739be
nkhatri558 <nkhatri558@gmail.com>
2024-08-07 23:40:22 +0200

    API URL Update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://centralloadbalancer-1924056286.eu-central-1.elb.amazonaws.com/api',
+    baseURL: 'centralloadbalancer-1924056286.eu-central-1.elb.amazonaws.com:8080/api',
 });
